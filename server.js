12c582a6b986d491f254f3bf5e1fe955ee4b0bad
tommy martinez <thomasjohnmartinez@gmail.com>
2023-05-07 14:53:02 -0400

    user state
    state machine and persistence?

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const express = require('express');
 const path = require('path');
 const app = module.exports.app = express();
 const port = process.env.PORT || 3000;
+let users = 0;
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '/index.html'));
@@ -20,11 +21,16 @@ const server = app.listen(port, () => {
 const io = socketIO(server);
 
 io.on('connection', (socket) => {
+  users +=1;
+  io.emit('connecty', 'a new user joined at ');
+  io.emit('numUsers', users);
   console.log('Client connected');
   socket.on("controller", (arg) => {
     console.log(arg); //gust
     io.emit('response', arg);
   });
-  socket.on('disconnect', () => console.log('Client disconnected'), io.emit('left','a user left'));
+  socket.on('disconnect', () => io.emit('disconnected', 'a user left'));
+  socket.on('disconnect', () => users -=1);
+  socket.on('disconnect', () => io.emit('numUsers', users));
 });
 
